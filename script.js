79332f4df3eb2bc09fb8c40d511737e01cc5a70a
Cameron <camd.99@gmail.com>
2017-02-19 10:22:12 -0600

    Clean-up

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
 $(document).ready(function(){
-    $("#infoBtn").hover(
-        function(){
-            $(this).css("color","blue");
-            $("#info").toggle();
-        },
-        function(){
-            $(this).css("color","black");
-            $("#info").toggle();
-        });
 	$("#sidebarButton").on("click",function(){
 		toggleID("sidebar");
 		if(document.getElementById("arrowIcon").innerHTML=="keyboard_arrow_left")
 			document.getElementById("arrowIcon").innerHTML="keyboard_arrow_right";
 		else
 			document.getElementById("arrowIcon").innerHTML="keyboard_arrow_left";
-	});	
+	});
 });
 
 function toggleID(id){
 	$("#"+id).animate({width:'toggle'},350);
-}
-
-
-
-function updateInfo(JSON_obj){
-	
 }
